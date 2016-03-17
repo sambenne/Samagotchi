d5485c7fd6f1d2f37a767db39e61386b5fce82a5
Sam Bennett <bennettsst@gmail.com>
2016-03-17 21:06:22 +0000

    Added missing files

---
+++ Samagotchi/App/Pet/IPet.cs
@@ -0,0 +1,47 @@
+﻿namespace Samagotchi.App.Pet
+{
+    public interface IPet
+    {
+        string Type { get; set; }
+        string Name { get; set; }
+        Gender Gender { get; set; }
+        int Health { get; set; }
+        int Hunger { get; set; }
+        int Thirst { get; set; }
+        int Boredom { get; set; }
+        int Age { get; set; }
+        int MaxHunger { get; set; }
+
+        PetObject ToPetObject();
+        void Map(PetObject petObject);
+        void LowerHunger();
+        void LowerThirst();
+        void IncreaseBoredom();
+        void IncreaseAge();
+        bool Feed(string item);
+        bool Drink(string item);
+        bool Play(string game);
+        string CanEat();
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+
+    public class GenderTypes
+    {
+        public static Gender From(string sexInput)
+        {
+            switch (sexInput.ToLower())
+            {
+                case "male":
+                case "boy":
+                    return Gender.Male;
+                default:
+                    return Gender.Female;
+            }
+        }
+    }
+}
---
+++ Samagotchi/App/Pet/Pet.cs
@@ -0,0 +1,111 @@
+﻿using Samagotchi.App.Helpers;
+
+namespace Samagotchi.App.Pet
+{
+    public abstract class Pet : IPet
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+        public int Health { get; set; }
+        public int Hunger { get; set; }
+        public int Thirst { get; set; }
+        public int Boredom { get; set; }
+        public int Age { get; set; }
+        public int MaxHunger { get; set; }
+
+        public PetObject ToPetObject()
+        {
+            return new PetObject
+            {
+                Name = Name,
+                Type = Type,
+                Health = Health,
+                Hunger = Hunger,
+                Boredom = Boredom,
+                Thirst = Thirst,
+                Age = Age,
+                Gender = Gender
+            };
+        }
+
+        public void Map(PetObject petObject)
+        {
+            Name = petObject.Name;
+            Boredom = petObject.Boredom;
+            Health = petObject.Health;
+            Hunger = petObject.Hunger;
+            Thirst = petObject.Thirst;
+            Age = petObject.Age;
+        }
+
+        public void LowerHunger()
+        {
+            Hunger -= Hunger == 0 ? 0 : 1;
+            PetManager.Instance.Save();
+            if (Hunger == 0)
+            {
+                ConsoleHelpers.ErrorMessage(PetManager.Pet.Name + " is starving!");
+                LowerHealth();
+                ConsoleHelpers.PlayNote(Tone.A, Duration.Quarter);
+                return;
+            }
+
+            if (Hunger >= 3) return;
+            ConsoleHelpers.WarnMessage(PetManager.Pet.Name + " is getting really hungry!");
+            ConsoleHelpers.PlayNote(Tone.Dsharp, Duration.Quarter);
+        }
+
+        public void LowerThirst()
+        {
+            Thirst -= Thirst == 0 ? 0 : 1;
+            PetManager.Instance.Save();
+            if (Thirst == 0)
+            {
+                ConsoleHelpers.ErrorMessage(PetManager.Pet.Name + " is really thirsty!");
+                LowerHealth();
+                ConsoleHelpers.PlayNote(Tone.A, Duration.Quarter);
+                return;
+            }
+
+            if (Thirst >= 3) return;
+            ConsoleHelpers.WarnMessage(PetManager.Pet.Name + " is getting really thirsty!");
+            ConsoleHelpers.PlayNote(Tone.Dsharp, Duration.Quarter);
+        }
+
+        public void IncreaseBoredom()
+        {
+            Boredom += Boredom == 10 ? 0 : 1;
+            PetManager.Instance.Save();
+            if (Boredom == 10)
+            {
+                ConsoleHelpers.ErrorMessage(PetManager.Pet.Name + " is depressed!");
+                ConsoleHelpers.PlayNote(Tone.A, Duration.Quarter);
+                return;
+            }
+
+            if (Boredom <= 7) return;
+            ConsoleHelpers.WarnMessage(PetManager.Pet.Name + " is getting really bored!");
+            ConsoleHelpers.PlayNote(Tone.Dsharp, Duration.Quarter);
+        }
+
+        public void IncreaseAge()
+        {
+            Age += 1;
+        }
+
+        public void LowerHealth()
+        {
+            Health -= Health == 0 ? 0 : 1;
+            PetManager.Instance.Save();
+        }
+
+        public abstract bool Feed(string item);
+
+        public abstract bool Drink(string item);
+
+        public abstract bool Play(string game);
+
+        public abstract string CanEat();
+    }
+}
